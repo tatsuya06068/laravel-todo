00a375abe94abe9355dfc1f860fb83ffca9ada6c
tatsuya~ <fhkt863@gmail.com>
2022-08-24 07:50:28 +0900

    docker修正

--- src/README.md
+++ src/README.md
@@ -1,3 +1,6 @@
 composer create-project --prefer-dist "laravel/laravel=9.*" .
 
 npm i @vitejs/plugin-vue 
+
+<script type="module" src="http://0.0.0.0:3000/@vite/client"></script>
+<script type="module" src="http://0.0.0.0:3000/resources/ts/app.ts"></script>
--- src/resources/ts/App.vue
+++ src/resources/ts/App.vue
@@ -1,12 +1,13 @@
 <script lang="ts" setup>
     import { ref } from 'vue';
     const count = ref(0);
+    let a: Number;
 </script>
 
 <template>
   <div>
       <button @click="count++">
-            Count is: {{ count }}
+            Count is {{ count }}
         </button>
     </div>
 </template>
